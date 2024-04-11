c29ec273de16b25a5c6a6c85ee4dda25377b9ce4
onedataengineer21 <onedataengineer@gmail.com>
2024-04-11 00:18:31 -0500

    Updating the ETL Script

--- pipeline/src/etl_script.py
+++ pipeline/src/etl_script.py
@@ -115,4 +115,6 @@ def generate_covidreport_statewise(statename):
     
     
 if __name__ == "__main__":
-    generate_covidreport_statewise('Virgin Islands')
+    state_names = ["Alaska", "Alabama", "Arkansas", "American Samoa", "Arizona", "California", "Colorado", "Connecticut", "District ", "of Columbia", "Delaware", "Florida", "Georgia", "Guam", "Hawaii", "Iowa", "Idaho", "Illinois", "Indiana", "Kansas", "Kentucky", "Louisiana", "Massachusetts", "Maryland", "Maine", "Michigan", "Minnesota", "Missouri", "Mississippi", "Montana", "North Carolina", "North Dakota", "Nebraska", "New Hampshire", "New Jersey", "New Mexico", "Nevada", "New York", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Puerto Rico", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Virginia", "Virgin Islands", "Vermont", "Washington", "Wisconsin", "West Virginia", "Wyoming"]
+    for state in state_names:
+        generate_covidreport_statewise(state)
